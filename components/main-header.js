9d8eddd55c1335ec7659cb62ff55def7a7296d22
Vrigz Alejo <brigsalejo@gmail.com>
2024-10-19 17:11:25 +0800

    refactored nav links

--- components/main-header.js
+++ components/main-header.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import NavLink from './nav-link';
+
 export default function MainHeader() {
   return (
     <header id="main-header">
@@ -9,7 +11,10 @@ export default function MainHeader() {
       <nav>
         <ul>
           <li>
-            <Link href="/news">News</Link>
+            <NavLink href="/news">News</NavLink>
+          </li>
+          <li>
+            <NavLink href="/archive">Archive</NavLink>
           </li>
         </ul>
       </nav>
